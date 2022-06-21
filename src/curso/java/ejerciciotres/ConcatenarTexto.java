cece7543ec58eddf54d27ac3167bf4c777379e74
JoelTejerina <catrieltejerina@gmail.com>
2022-06-20 23:15:32 -0300

    Cambiando nombre main

--- src/curso/java/ejerciciotres/ConcatenarTexto.java
+++ src/curso/java/ejerciciotres/ConcatenarTexto.java
@@ -2,7 +2,7 @@ package curso.java.ejerciciotres;
 
 import java.util.Scanner;
 
-public class Main {
+public class ConcatenarTexto {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
